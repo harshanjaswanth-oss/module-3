cfa8bb00ff62bedd39b9922be989561c5be929bc
harshanjaswanth-oss <Jaswanth902>
2026-03-25 15:48:32 +0530

    math.operation

---
+++ math operations.py
@@ -0,0 +1,17 @@
+import math
+
+print(math.pow(4,5))
+
+print(math.sqrt(90))
+
+print(math.ceil(20.6))
+
+print(math.floor(20.6))
+
+print(math.pi)
+
+print(math.factorial(8))
+
+print(math.gcd(12,24))                    
+                
+               
